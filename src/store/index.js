3b1131f7d2c59f56103ae4c5086ee7df282045ab
wesley.wg <wesley.magalhaes@webglobal.com.br>
2022-01-28 15:02:19 -0300

    Vue

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+    state: {
+        user: {
+            data: {
+                name: "Zura"
+            },
+            token: null,
+        },
+    },
+    getters: {},
+    actions: {},
+    mutations: {},
+    modules: {},
+});
+
+export default store; 
