05ce7b2d5d669637442e55927898a050bf04389d
wesley.wg <wesley.magalhaes@webglobal.com.br>
2022-02-23 15:56:50 -0300

    async components and lazy loading pages on routes

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-import { createApp } from 'vue';
+import { createApp, defineAsyncComponent } from 'vue';
 
 import router from './router';
 import store from './store/index';
 import './index.css';
 
 import App from './App.vue';
-import BaseCard from './components/ui/BaseCard.vue';
-import BaseButton from './components/ui/BaseButton.vue';
-import BaseBadge from './components/ui/BaseBadge.vue';
-import BaseSpinner from './components/ui/BaseSpinner.vue';
-import BaseDialog from './components/ui/BaseDialog.vue';
+
+const BaseCard = defineAsyncComponent(() => import('./components/ui/BaseCard.vue'));
+const BaseButton = defineAsyncComponent(() => import('./components/ui/BaseButton.vue'));
+const BaseBadge = defineAsyncComponent(() => import('./components/ui/BaseBadge.vue'));
+const BaseSpinner = defineAsyncComponent(() => import('./components/ui/BaseSpinner.vue'));
+const BaseDialog = defineAsyncComponent(() => import('./components/ui/BaseDialog.vue'));
 
 const app = createApp(App);
 
--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDetail from './pages/coaches/CoachDetail.vue';
-import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistation from './pages/coaches/CoachRegistration.vue';
-import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestsReceived from './pages/requests/RequestsReceived.vue';
-import NotFound from './pages/NotFound.vue';
+const CoachDetail = () => import('./pages/coaches/CoachDetail.vue');
+const CoachesList = () => import('./pages/coaches/CoachesList.vue');
+const CoachRegistation = () => import('./pages/coaches/CoachRegistration.vue');
+const ContactCoach = () => import('./pages/requests/ContactCoach.vue');
+const RequestsReceived = () => import('./pages/requests/RequestsReceived.vue');
+const NotFound = () => import('./pages/NotFound.vue');
 
 const router = createRouter({
   history: createWebHistory(),
